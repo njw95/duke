4eb30972f080f50eb0738784458c6e37dc31a8a9
njw95 <ngjianwei@u.nus.edu>
2019-08-30 10:50:34 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,11 +14,18 @@ public class Duke {
     }
     public static void echo() {
         String myCommand="";
+        List<String> mylist = new ArrayList<>();
         while(true){
             Scanner sc = new Scanner(System.in);
             myCommand = sc.nextLine();
             if(myCommand.equals("bye"))break;
-            else System.out.println(myCommand);
+            else if(myCommand.equals("list")){
+                mylist.forEach(System.out::println);
+            }
+            else {
+                mylist.add(myCommand);
+                System.out.println("added: "+myCommand);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
