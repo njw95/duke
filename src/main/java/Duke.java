ff2fb3dda0b57c07254d3f882a31d90aa0ba8532
njw95 <ngjianwei@u.nus.edu>
2019-08-31 12:17:01 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,11 +18,11 @@ public class Duke {
             Scanner sc = new Scanner(System.in);
             String myInstruction = sc.nextLine();
 
-            if (myInstruction.equals("bye")) {
+            if (myInstruction.equals("bye")) { //bye
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            else if (myInstruction.equals("list")) { //list out everything
+            else if (myInstruction.equals("list")) { //list now supports Symbol T,E,D on top of the tick/cross
                 if (myList.isEmpty()) System.out.println("List is empty."); //list empty
                 else { //list has items
                     int counter = 0;
@@ -30,20 +30,51 @@ public class Duke {
                     for (Task task : myList) { //for all task in myList, traverse and print
                         // [0] is the taskName, [1] is the symbol
                         counter++;
-                        System.out.println(counter + ".[" + task.getStatusIcon() + "] " + task.description);
+                        if(task.letterSymbol.equals("T"))
+                            System.out.println(counter + ".[" + task.letterSymbol + "] " + "["+task.getStatusIcon()+"] "+task.description);
+                        else {
+                            String[] taskDatesplit = task.date.split("\\s", 2); //split "by day/full date"
+                            System.out.println(counter + ".[" + task.letterSymbol + "] " + "[" + task.getStatusIcon() + "] " + task.description + " (" + taskDatesplit[0] + ": " + taskDatesplit[1]+")");
+                        }
                     }
                 }
             }
-            else if (myInstruction.contains("done")) { //mark task number as done
-                String words[] = myInstruction.split("\\s"); //splits the string based on whitespace
+            else if (myInstruction.contains("done")) { //done
+                String[] words = myInstruction.split("\\s");
                 myList.get(Integer.parseInt(words[1]) - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("[" + myList.get(Integer.parseInt(words[1]) - 1).getStatusIcon() + "] " + myList.get(Integer.parseInt(words[1]) - 1).description);
             }
-            else { //adding tasks with undone marker/cross symbol
-                Task newTask = new Task(myInstruction);
-                myList.add(newTask);
-                System.out.println("added: " + myInstruction);
+            else if(myInstruction.contains("todo")){
+                String[] words = myInstruction.split("\\s",2);
+                Task newToDoTask = new Task(words[1],"T");
+                myList.add(newToDoTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("["+ newToDoTask.letterSymbol+"]"+"["+newToDoTask.getStatusIcon()+"] "+words[1]);
+                System.out.println("Now you have "+myList.size()+" tasks in the list.");
+            }
+            else if(myInstruction.contains("deadline")){
+                String[] words = myInstruction.split("\\s",2); //"deadline" and "instruction /by day"
+                String[] TaskDesc = words[1].split("/",2); //"instruction" and "by day/full date"
+                String[] TaskDate = TaskDesc[1].split("\\s",2); //"by" and "day"
+                Task newDeadlineTask = new Task(TaskDesc[0],"D",TaskDesc[1]);
+                myList.add(newDeadlineTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("["+ newDeadlineTask.letterSymbol+"]"+"["+newDeadlineTask.getStatusIcon()+"] "+TaskDesc[0]+"("+TaskDate[0]+": "+TaskDate[1]+")");
+                System.out.println("Now you have "+myList.size()+" tasks in the list.");
+            }
+            else if(myInstruction.contains("event")){
+                String[] words = myInstruction.split("\\s",2); //"event" and "instruction /by day"
+                String[] TaskDesc = words[1].split("/",2); //"instruction" and "at day/full date"
+                String[] TaskDate = TaskDesc[1].split("\\s",2); //"by" and "day"
+                Task newEventTask = new Task(TaskDesc[0],"E",TaskDesc[1]);
+                myList.add(newEventTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("["+ newEventTask.letterSymbol+"]"+"["+newEventTask.getStatusIcon()+"] "+TaskDesc[0]+" ("+TaskDate[0]+": "+TaskDate[1]+")");
+                System.out.println("Now you have "+myList.size()+" tasks in the list.");
+            }
+            else { //Normal adding no longer works without proper instruction
+                System.out.println("Please use proper instruction "+ "\"todo\""+ ", " + "\"deadline\"" +"or"+ "\"event\"");
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,17 +1,27 @@
 public class Task {
     protected String description;
+    protected String letterSymbol;
+    protected String date;
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, String letterSymbol) {
         this.description = description;
         this.isDone = false;
+        this.letterSymbol = letterSymbol; //T,D, and E
+    }
+
+    public Task(String description, String letterSymbol, String date) {
+        this.description = description;
+        this.isDone = false;
+        this.letterSymbol = letterSymbol; //T,D, and E
+        this.date = date;
     }
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    public void markAsDone() {
+    public void markAsDone(){
         this.isDone = true;
     }
 }
