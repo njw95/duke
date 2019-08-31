a01dbdbe4d56858ae0c59637c4a0c954018315e6
njw95 <ngjianwei@u.nus.edu>
2019-08-31 09:45:44 +0800

    Extension A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,31 +12,36 @@ public class Duke {
         System.out.println(logo);
         System.out.println("Hello I'm Duke\nWhat Can I do for you?");
 
-        String myInstruction = "";
-        ArrayList<String[]> myList = new ArrayList<String[]>();
+        ArrayList<Task> myList = new ArrayList<Task>();
+
         while (true) {
             Scanner sc = new Scanner(System.in);
-            myInstruction = sc.nextLine();
+            String myInstruction = sc.nextLine();
+
             if (myInstruction.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (myInstruction.equals("list")) { //list out everything
+            }
+            else if (myInstruction.equals("list")) { //list out everything
                 if (myList.isEmpty()) System.out.println("List is empty."); //list empty
                 else { //list has items
                     int counter = 0;
                     System.out.println("This is your current list.");
-                    for (String[] task : myList) { //[1] is the symbol, [0] is the item
+                    for (Task task : myList) { //for all task in myList, traverse and print
+                        // [0] is the taskName, [1] is the symbol
                         counter++;
-                        System.out.println(counter + ".[" + task[1] + "] " + task[0]);
+                        System.out.println(counter + ".[" + task.getStatusIcon() + "] " + task.description);
                     }
                 }
-            } else if (myInstruction.contains("done")) { //mark task number as done
-                String keyword[] = myInstruction.split(" "); //split the words into different strings
-                myList.get(Integer.parseInt(keyword[1]) - 1)[1] = "✓"; //find the [i]-1 element in the list and get symbol
+            }
+            else if (myInstruction.contains("done")) { //mark task number as done
+                String words[] = myInstruction.split("\\s"); //splits the string based on whitespace
+                myList.get(Integer.parseInt(words[1]) - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + myList.get(Integer.parseInt(keyword[1]) - 1)[1] + "] " + myList.get(Integer.parseInt(keyword[1]) - 1)[0]);
-            } else {
-                String[] newTask = {myInstruction, "✗"};
+                System.out.println("[" + myList.get(Integer.parseInt(words[1]) - 1).getStatusIcon() + "] " + myList.get(Integer.parseInt(words[1]) - 1).description);
+            }
+            else { //adding tasks with undone marker/cross symbol
+                Task newTask = new Task(myInstruction);
                 myList.add(newTask);
                 System.out.println("added: " + myInstruction);
             }
